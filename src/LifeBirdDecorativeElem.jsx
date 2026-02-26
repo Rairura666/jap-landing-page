23df935720976a3047c23b928f92f5ae87633b95
Rairura <biryukova.ira63@gmail.com>
2026-02-26 16:36:46 +0300

    Adaptive design finished.

--- src/LifeBirdDecorativeElem.jsx
+++ src/LifeBirdDecorativeElem.jsx
@@ -4,14 +4,14 @@ export function LifeBirdDecorativeElem() {
             <div className="lifeBirdElemIconsRow">
                 <span className="sushiIconEight">3</span>
                 <div className="sushiIconEyeContainer">
-                    <svg className="cameraIconBird" width="2rem" height="2rem" viewBox="0 0 1109 919" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M421.619 15L690.31 15.001C709.639 15.0012 725.309 30.6713 725.31 50.001V56.917C725.31 92.8153 754.411 121.917 790.31 121.917H1059C1078.33 121.917 1094 137.587 1094 156.917V868.5C1094 887.83 1078.33 903.5 1059 903.5H50C30.67 903.5 15 887.83 15 868.5V156.917C15 137.587 30.67 121.917 50 121.917H321.618C357.517 121.917 386.618 92.8153 386.618 56.917V50C386.618 30.6701 402.289 14.9999 421.619 15Z" stroke="white" strokeWidth="5rem" />
-                        <circle cx="554.5" cy="498" r="216.5" stroke="white" strokeWidth="5rem" />
+                    <svg className="cameraIconBird" width="2rem" height="2rem" viewBox="-50 0 1200 919" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M421.619 15L690.31 15.001C709.639 15.0012 725.309 30.6713 725.31 50.001V56.917C725.31 92.8153 754.411 121.917 790.31 121.917H1059C1078.33 121.917 1094 137.587 1094 156.917V868.5C1094 887.83 1078.33 903.5 1059 903.5H50C30.67 903.5 15 887.83 15 868.5V156.917C15 137.587 30.67 121.917 50 121.917H321.618C357.517 121.917 386.618 92.8153 386.618 56.917V50C386.618 30.6701 402.289 14.9999 421.619 15Z" stroke="black" strokeWidth="5rem" />
+                        <circle cx="554.5" cy="498" r="216.5" stroke="black" strokeWidth="5rem" />
                     </svg>
                 </div>
             </div>
 
-            <svg className="lifeBirdIcon" width="15rem" height="15rem" viewBox="0 0 1511 1511" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className="lifeBirdIcon" viewBox="0 0 1511 1511" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="755.5" cy="755.5" r="755.5" fill="white" />
                 <path d="M1264.5 785C1243.5 1116.5 1030.48 1226.96 768.5 1287C487.5 1287 240 1054.25 240 758.5C240 462.752 467.795 223 748.795 223C1029.79 223 1285.5 453.5 1264.5 785Z" fill="#E23E32" />
                 <path d="M1272 753C1226.5 997 1077 1174 765 1260C484.992 1260 258 1033.01 258 753C258 472.992 484.992 246 765 246C1045.01 246 1272 472.992 1272 753Z" fill="#E23E32" />
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,18 +7,20 @@ export function NavBar() {
     return (
         <>
             <div id="navbar">
-                <a
-                    href="#"
-                    onClick={() => setChosenElem("main")}
-                ><span className={chosenElem === "main" ? "chosenA" : ""}>MAIN</span></a>
-                <a
-                    href="#about"
-                    onClick={() => setChosenElem("about")}
-                ><span className={chosenElem === "about" ? "chosenA" : ""}>ABOUT</span></a>
-                <a
-                    href="#contact"
-                    onClick={() => setChosenElem("contact")}
-                ><span className={chosenElem === "contact" ? "chosenA" : ""}>CONTACT</span></a>
+                <div className="navWrapper">
+                    <a
+                        href="#"
+                        onClick={() => setChosenElem("main")}
+                    ><span className={chosenElem === "main" ? "chosenA" : ""}>MAIN</span></a>
+                    <a
+                        href="#about"
+                        onClick={() => setChosenElem("about")}
+                    ><span className={chosenElem === "about" ? "chosenA" : ""}>ABOUT</span></a>
+                    <a
+                        href="#contact"
+                        onClick={() => setChosenElem("contact")}
+                    ><span className={chosenElem === "contact" ? "chosenA" : ""}>CONTACT</span></a>
+                </div>
             </div>
 
         </>
