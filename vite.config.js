47764542fa1cbe9c11430f9088a0a98d0f1bf883
Rairura <biryukova.ira63@gmail.com>
2026-02-26 19:34:02 +0300

    Minor fixes.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({mode})=> {
+
+  const isProd = mode === 'production'
+
+    return {
+      base: isProd ? '/jap-landing-page/' : '/',
+      plugins: [react()],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      }
+    }
+  }
+)
